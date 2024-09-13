bdb44de97261552812f91cf6f2eac913512cabe8
charishma-Rongali <charishmarongali354@gmail.com>
2024-09-13 15:29:02 +0530

    Update hangman game UI and functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
+let inputFields;
+
 document.addEventListener("DOMContentLoaded", () => {//loaded after the whole documents is loaded mostly used when we use inputs and buttons
   const {word,riddle}=riddles[Math.floor(Math.random()*riddles.length)];
   document.querySelector(".hint-text b").innerHTML=riddle;
+  document.querySelector(".content b").innerHTML=word;
   console.log(word,riddle);
   const wordLetters = word.split("");
-  const inputFields = document.querySelectorAll(".input-underline"); 
+  inputFields = document.querySelectorAll(".input-underline"); 
   const buttons = document.querySelectorAll(".keyboard button"); 
   let wrongGuess=0;
   buttons.forEach(button => {
@@ -20,14 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {//loaded after the whole do
           else{
             wrongGuess+=1
             console.log(wrongGuess);
+            const chanceElement = document.querySelector('.chances b');
+            let currentValue = parseInt(chanceElement.textContent, 10);//converts the current text(string) to a number.
+            chanceElement.textContent = currentValue + 1;
           }
           if(wrongGuess==6){
-            document.getElementsByClassName("game-model")[0].style.display="flex";
+            document.getElementsByClassName("game-model")[0].style.display="flex";//ClassName returns a collection of elements
           }
           button.disabled = true;
       });
   });
 });
 
+const playAgain=document.querySelector(".play-again");
+playAgain.addEventListener("click",()=>{
+  document.getElementsByClassName("game-model")[0].style.display="none";
+  const chanceElement = document.querySelector('.chances b');
+  let currentValue = parseInt(chanceElement.textContent, 10);
+  chanceElement.textContent = 0;
+  inputFields.forEach(input=>{
+    input.value='';
+  })
+});
 
   
